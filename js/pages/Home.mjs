9264c170d63fc21ae4ee0f562e2ce5b69436b0e2
Adrian Ani <ani.adrian99andrei@gmail.com>
2022-03-04 06:17:08 +0000

    changed logo hover animation a bit

--- js/pages/Home.mjs
+++ js/pages/Home.mjs
@@ -1,5 +1,7 @@
 const HomePage = document.createElement('main');
 
 HomePage.appendChild(document.createTextNode('Home page'));
+HomePage.appendChild(document.createElement('br'));
+HomePage.appendChild(document.createTextNode('Home page'));
 
 export default HomePage;
